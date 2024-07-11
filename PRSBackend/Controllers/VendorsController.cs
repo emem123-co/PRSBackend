9dbe54c145aafb1b378b90167355e61e7ec248f4
Emily Morgan <emorgan921@gmail.com>
2024-07-11 11:54:07 -0400

    added Po and Polines class. added CreatePo method to VendorsController.

--- PRSBackend/Controllers/VendorsController.cs
+++ PRSBackend/Controllers/VendorsController.cs
@@ -31,7 +31,7 @@ namespace PRSBackend.Controllers
             
             newVendor = po.Vendor;
 
-            var polines = (from v in _context.Vendors 
+            var poLines = (from v in _context.Vendors 
                           join p in _context.Products
                             on v.Id equals p.VendorId
                           join rql in _context.RequestLines 
@@ -48,29 +48,38 @@ namespace PRSBackend.Controllers
                             LineTotal = (p.Price * rql.Quantity)
                           });
 
-            var sortedLines = new SortedList<int, Poline>(); //new collection with int as key and Poline as data.
+            var sortedLines = new SortedList<int, Poline>(); //new list with int key and Poline as data.
 
-            //foreach(var line in sortedLines)
-            //{
-            //    if(!sortedLines.ContainsKey(line.Key))
-            //    {
-            //        var poline = new Poline()
-            //        {
-            //            Product = l.Product
-            //            Quantity = 0
-            //            Price = l.Price
-            //            LineTotal = 
+            foreach(var poLine in poLines) //iterate through each item in the select statement collection once.
+            {
+                if(!sortedLines.ContainsKey(poLine.Id)) //if the product id from select statement collection is NOT already in the new list...
+                {
+                    var newPo = new Poline() //...create new data for Poline list and store into variable newPo
+                    {
+                        Product = poLine.Product, 
+                        Quantity = 0, //sets the Quantity to 0.
+                        Price = poLine.Price,
+                        LineTotal = poLine.LineTotal
+                    };
+                sortedLines.Add(poLine.Id, newPo); //...and add the new data (newPo) and key (poLine.Id) that will be the productID from the select statement.
+                   
+                   //then, the new line is in the List with key = poLine.Id. and quantity = 0.
+                }
 
-            //        };
-            //    }
-            //}
-            
+            //items added to the list have Quantity = 0. The below statement accesses the Quantity of the associated key in the new list and increments it by the value of Quantity from the poLine select statment.
 
-            
+            sortedLines[poLine.Id].Quantity += poLine.Quantity;
+            }
+            //then it moves to the next item in poLine and repeats the process. The process stops once all items have been dealt with. 
 
-            
-            
-        return Ok();   
+                        //why is poLine.Id not named poLine.key? because it points to the primary key of products which will inherently be unique for each item in the select statment. 
+
+                        //why is the quantity not set to 1? because the true quanity will be added at the end (from the quantity value that lives in the select statment) and 0 allows them to match. 
+
+            po.Polines = (sortedLines).Values; //fill Po.Polines property with the sortedLines collection.
+            po.PoTotal = po.Polines.Sum(x => x.LineTotal); //Calculate Po total 
+
+            return Ok();  
         }
 
         // GET: api/Vendors
